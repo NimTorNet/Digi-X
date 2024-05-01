efa910c9df4dd414d1f200495f12daf744c67e95
NimTorNet <103305148+NimTorNet@users.noreply.github.com>
2024-05-01 14:10:08 +0330

    Update script.js

--- script.js
+++ script.js
@@ -5,3 +5,10 @@ function playVideo(link) {
     videoElement.load();
     videoElement.play();
 }
+
+function playVideo(link) {
+    var videoElement = document.getElementById('video');
+    videoElement.src = link;
+    videoElement.load();
+    videoElement.play();
+}
